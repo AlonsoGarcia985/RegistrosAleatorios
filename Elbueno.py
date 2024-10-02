4a486d68c49885337fac84b572fdc68cadbabcd0
AlonsoGarcia985 <alonsogarciam04@gmail.com>
2024-10-02 14:28:40 -0600

    Add files via upload

---
+++ Elbueno.py
@@ -0,0 +1,38 @@
+from faker import Faker
+import mysql.connector  
+
+ 
+db = mysql.connector.connect(
+    host="localhost",       
+    user="root",      
+    password="root123", 
+    database="Empresa"
+)
+
+cursor = db.cursor()
+
+fake = Faker()
+
+def insert_Departamentos(n):
+    print('entra')
+    for i in range(n):
+        nombre=fake.first_name()
+        
+        cursor.execute(f"INSERT INTO Departamentos (Nombre) VALUES ('{nombre}')")
+
+def insert_empleados(n):
+    for i in range(n):
+        nombre = fake.first_name()
+        apellido = fake.last_name()
+        email = fake.unique.email()
+        telefono = fake.phone_number()
+        fecha_contratacion = fake.date_between(start_date='-5y', end_date='today')
+        
+        cursor.execute(f"insert into Empleados (Nombre, Apellido, Email, Telefono, Fecha_Contratacion) Values ('{nombre}','{apellido}','{email}','{telefono}','{fecha_contratacion}')") 
+
+
+#insert_Departamentos(50000)
+insert_empleados(50000) 
+db.commit()
+cursor.close()
+db.close()
